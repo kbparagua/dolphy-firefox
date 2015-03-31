debc2ccd83dfc9dc56cc2e6e787dbdb9e7c1dbbb
kbparauga <kb.paragua@gmail.com>
2015-04-01 01:06:57 +0800

    working image source. yay

--- data/js/dolphy.js
+++ data/js/dolphy.js
@@ -13,7 +13,20 @@
   };
 
 
-  global._Dolphy.setSource = function(source){
+  global._Dolphy.setSource = function(page){
+    var source = {};
+
+    var $page = $('<div>');;
+    $page.html(page);
+
+    var $markdown = $page.find('.blob:first'),
+        $imgs = $markdown.find('img');
+
+    $imgs.each(function(_, img){
+      var $img = $(img);
+      source[ $img.prop('alt') ] = $img.prop('src');
+    });
+
     this._source = source;
   };
 
@@ -25,8 +38,10 @@
 
 
 
-  self.port.on('setSource', function(source){
-    global._Dolphy.setSource(source);
+
+
+  self.port.on('setSourceFromPage', function(page){
+    global._Dolphy.setSource(page);
   });
 
 
